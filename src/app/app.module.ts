c8f3b45c948a4999b13bc3145d053d20b25a70e1
Karekar <omkar.santosh-karekar@capgemini.com>
2023-06-06 17:19:24 +0530

    added bootstrap and created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './component/todolist/todolist.component';
+import { TodoinputComponent } from './component/todoinput/todoinput.component';
+import { TodoitemsComponent } from './component/todoitems/todoitems.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent,
+    TodoinputComponent,
+    TodoitemsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/component/todoinput/todoinput.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todoinput',
+  templateUrl: './todoinput.component.html',
+  styleUrls: ['./todoinput.component.css']
+})
+export class TodoinputComponent {
+
+}
