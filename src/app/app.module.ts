43466e840c411ef1386476a5c39504705dc10532
Omkar1026 <omkarkarekar1998@gmail.com>
2023-06-17 17:23:40 +0530

    Revert "added bootstrap and created components"
    This reverts commit c8f3b45c948a4999b13bc3145d053d20b25a70e1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TodolistComponent } from './component/todolist/todolist.component';
-import { TodoinputComponent } from './component/todoinput/todoinput.component';
-import { TodoitemsComponent } from './component/todoitems/todoitems.component';
-import { FormsModule } from '@angular/forms';
-import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TodolistComponent,
-    TodoinputComponent,
-    TodoitemsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
